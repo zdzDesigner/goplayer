d0eabb666b9439616c7e45b4931a6ae5c688b026
zdzDesigner <1696498456@qq.com>
2022-05-04 10:16:42 +0800

    add run.sh

--- pprof.go
+++ pprof.go
@@ -4,8 +4,13 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
 )
 
 func pprof() {
+	if os.Getenv("__ENV__") != "DEV" {
+		return
+	}
+
 	log.Println(http.ListenAndServe(":6060", nil))
 }
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+arg=$1
+ENV=${arg#*=}
+# echo "${ENV^^}"
+
+if [[ $1 ]]; then
+export __ENV__="${ENV^^}"
+fi
+
+
+go run .
